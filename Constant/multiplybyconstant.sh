090b9e7c99af7829d52dee62d23c9b4fa7ced164
Justine Dela Torre <97047918+Jasut1n@users.noreply.github.com>
2022-05-09 20:36:40 +0800

    Arithmetic Constant

---
+++ Constant/multiplybyconstant.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+printf "_-_---__--_-_-_---_--_-_--__-_---_-_-__-\n\t\t"
+read -p "X: " X
+
+printf "_-_--_-__---_-_-_--_-__--_-__---_-_-__--_\n\t\t"
+read -p "X*: " Y
+
+printf "\e[1;32m\n\t   Calculating...\e[0m"
+sleep 1
+printf "\e[1;32m\n\t**..................\e[0m"
+sleep 0
+printf "\e[1;32m\n\t****................\e[0m"
+sleep 0
+printf "\e[1;32m\n\t******..............\e[0m"
+sleep 0
+printf "\e[1;32m\n\t********............\e[0m"
+sleep 0
+printf "\e[1;32m\n\t**********..........\e[0m"
+sleep 0
+printf "\e[1;32m\n\t************........\e[0m"
+sleep 0
+printf "\e[1;32m\n\t**************......\e[0m"
+sleep 0
+printf "\e[1;32m\n\t****************....\e[0m"
+sleep 0
+printf "\e[1;32m\n\t******************..\e[0m"
+sleep 1
+printf "\e[1;32m\n\t********************\n\e[0m"
+
+printf "\nMultiplication of x by constant x* // "
+
+printf "%f\\n" $(( X *= Y ))
+printf "\t"
+
+printf "\n"
+printf "\e[1;36mHello \e[0m"$USER,
+printf "\e[1;36m You're currently using \e[0m"$SHELL
+
